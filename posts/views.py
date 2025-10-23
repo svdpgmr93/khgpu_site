54635b93064591495dfb31ee411f57a099cd9859
svdpgmr93 <73833558+svdpgmr93@users.noreply.github.com>
2025-10-23 15:09:25 +0300

    New files structura and added struct.html. Added all views func and urls path on 'Common' part

--- posts/views.py
+++ posts/views.py
@@ -10,14 +10,49 @@ def main(requests):
 def index(requests):
     return render(requests, 'posts/index.html')
 
-def education_organization(requests):
-    return render(requests, 'posts/education-organization.html')
+def sveden(requests):
+    return render(requests, 'sveden.html')
 
-def basic_info(requests):
-    return render(requests, 'posts/basic-info.html')
+def common(requests):
+    return render(requests, 'common.html')
 
-def documents(requests):
+def struct(requests):
+    return render(requests, 'struct.html')
+
+def document(requests):
     return render(requests, 'documents.html')
+
+def education(requests):
+    return render(requests, 'education.html')
+
+def eduStandarts(requests):
+    return render(requests, 'eduStandarts.html')
+
+def managers(requests):
+    return render(requests, 'managers.html')
+
+def employees(requests):
+    # if HttpResponseNotFound:
+    #     return render(requests,'404.html')     # Рабочая тема!
+    return render(requests, 'employees.html')
+
+def objects(requests):
+    return render(requests, 'objects.html')
+
+def grants(requests):
+    return render(requests, 'grants.html')
 
 def paid_edu(requests):
     return render(requests, 'paid-edu.html')
+
+def budget(requests):
+    return render(requests, 'budget.html')
+
+def vacant(requests):
+    return render(requests, 'vacant.html')
+
+def inter(requests):
+    return render(requests, 'inter.html')
+
+def catering(requests):
+    return render(requests, 'catering.html')
