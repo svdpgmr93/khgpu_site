e2dac4d5e71b6e9cdbf121595438870ec29dbb4f
svdpgmr93 <73833558+svdpgmr93@users.noreply.github.com>
2026-06-12 14:51:56 +0300

    Reformating models of education app, configure admin

--- education/admin.py
+++ education/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from .models import *
 
+class KafedraAdmin(admin.ModelAdmin):
+    list_display = ("name", "institut")
 
-admin.site.register(EduProgram)
+
+class EduProgramAdmin(admin.ModelAdmin):
+    list_display = ("name", "kafedra_name", "level")
+
+
+admin.site.register(EduProgram, EduProgramAdmin)
 admin.site.register(EduProgramAdvantageText)
 admin.site.register(Suitable)
 admin.site.register(KeySkills)
 admin.site.register(ProgramStructure)
 admin.site.register(Position)
 admin.site.register(Perspective)
+admin.site.register(EduProgramEduForm)
+admin.site.register(EduForms)
+admin.site.register(Institut)
+admin.site.register(Kafedra, KafedraAdmin)
